f245e774193cba966f874da30ae874accb587970
Daniel Ivan Martinez R <out_dmartinez@findep.com.mx>
2024-06-13 00:58:56 -0600

    test

--- src/test/java/com/app/SpringSecurityAppApplicationTests.java
+++ src/test/java/com/app/SpringSecurityAppApplicationTests.java
@@ -1,13 +1,19 @@
 package com.app;
 
+import com.app.repositories.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application.properties")
 class SpringSecurityAppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		UserRepository userRepository;
+		Assertions.assertNull(null);
 	}
 
 }
